295aa2abeb5f8480cd8789f908510aa8f247a279
Pradeep Viswanath S <pradeepviswarox@gmail.com>
2024-06-11 10:20:27 +0530

    feat: new project

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2b6b8fb3f0mshce3d285875d9238p1a4f32jsnc824725d0b58",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "10cc70d7a417381d43439c71fc835c75"; // enter your key from openweather API
